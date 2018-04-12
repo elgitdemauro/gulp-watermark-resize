67e89c246e0cad06c58387a5e8dea8a55c716316
Mauricio Avendaño <>
2018-04-12 16:48:30 -0300

    script massive rename

---
+++ ren.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mv chao_003151.jpg hola_003151.jpg 
+mv chao_003152.jpg hola_003152.jpg 
+mv chao_003153.jpg hola_003153.jpg 
+mv chao_003154.jpg hola_003154.jpg 
+mv chao_003155.jpg hola_003155.jpg 
+echo "Fin"
